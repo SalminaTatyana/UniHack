138954b2d5f00f70c4f7736751c07c83c43e0750
Salmina_Tatyana <ynitaz24@mail.ru>
2021-12-04 11:59:16 +0400

    куча измений

--- UniHackStart/Controllers/TeachersController.cs
+++ UniHackStart/Controllers/TeachersController.cs
@@ -1,15 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace UniHackStart.Controllers
 {
     public class TeachersController : Controller
     {
+        public string UserRole()
+        {
+            string userRole;
+            string path = @"C:\Институт\Uni\UniHackStart\wwwroot\role\Role.txt";
+            //using (FileStream fstream = new FileStream(path, FileMode.Open))
+            //{
+            //    byte[] output = new byte[1];
+            //    fstream.Read(output, 0, output.Length);
+            //    userRole = Encoding.Default.GetString(output);
+            //    fstream.Close();
+            //}
+            using (StreamReader fstream = new StreamReader(path))
+            {
+                byte[] output = new byte[1];
+                userRole = fstream.ReadLine();
+
+                fstream.Close();
+            }
+            if (userRole == "1")
+            {
+                StreamWriter writer = new StreamWriter(path);
+                writer.Close();
+                return "1";
+            }
+            if (userRole == "2")
+            {
+                StreamWriter writer = new StreamWriter(path);
+                writer.Close();
+                return "2";
+            }
+            return "0";
+        }
         public IActionResult Index()
         {
+            var role = UserRole();
+            ViewBag.Role = role;
             return View();
         }
     }
--- UniHackStart/Models/WeekTimetableModel.cs
+++ UniHackStart/Models/WeekTimetableModel.cs
@@ -5,20 +5,13 @@ using System.Threading.Tasks;
 
 namespace UniHackStart.Models
 {
-    public class TimetableModel
+    public class WeekTimetableModel
     {
+        public string dateOfWeek { get; set; }
+        public int weekNumber { get; set; }
         public string name { get; set; }
         public string teachername { get; set; }
         public string time { get; set; }
         public string place { get; set; }
-
-    }
-    public class TimetableDayModel
-    {
-        public string dateOfWeek { get; set; }
-        public int weekNumber { get; set; }
-        public TimetableModel lesson { get; set; }
-        
-
     }
 }
