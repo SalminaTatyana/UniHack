0f04c501ed415b3a5bfba6f107e102166e26ba2a
Salmina_Tatyana <ynitaz24@mail.ru>
2021-12-08 22:30:51 +0400

    Страница добавления расписания путь /log/adminlog

--- UniHackStart/Controllers/LogController.cs
+++ UniHackStart/Controllers/LogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -6,17 +7,41 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniHackStart.Models;
 
 namespace UniHackStart.Controllers
 {
     public class LogController : Controller
     {
-       public IActionResult Index()
+        //ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
+        public LogController(/*ApplicationContext context,*/ IWebHostEnvironment appEnvironment)
+        {
+            //_context = context;
+            _appEnvironment = appEnvironment;
+        }
+        public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                string path = "/Files/" + uploadedFile.FileName;
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                //_context.Files.Add(file);
+                //_context.SaveChanges();
+            }
+            return RedirectToAction("AdminLog");
+        }
         public IActionResult AdminLog() {
-            return View();
+            return View(/*_context.Files.ToList()*/);
         }
     }
 }
