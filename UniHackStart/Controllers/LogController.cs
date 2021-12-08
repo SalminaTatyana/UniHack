30bcc6c47f59149bca6700f20ed27466a025b7c2
DESKTOP-BMCT3CB\79084 <Maxim300401@mail.ru>
2021-12-08 21:52:01 +0400

    Загрузка файла на сервер

--- UniHackStart/Controllers/LogController.cs
+++ UniHackStart/Controllers/LogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -6,14 +7,38 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniHackStart.Models;
 
 namespace UniHackStart.Controllers
 {
     public class LogController : Controller
     {
-       public ActionResult Index()
+        ApplicationContext _context;
+        IWebHostEnvironment _appEnvironment;
+        public LogController(ApplicationContext context, IWebHostEnvironment appEnvironment)
         {
-            return View();
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+        public ActionResult Index()
+        {
+            return View(_context.Files.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                string path = "/Files/" + uploadedFile.FileName;
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path,FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Log");
         }
         public ActionResult AdminLog() {
             return View();
