89aa0f04b8e9cc547a2b4eba991129dc60dcfcf0
Salmina_Tatyana <ynitaz24@mail.ru>
2021-12-09 20:09:08 +0400

    Сессии complite edition

--- UniHackStart/Controllers/RoleController.cs
+++ UniHackStart/Controllers/RoleController.cs
@@ -22,11 +22,35 @@ namespace UniHackStart.Controllers
             }
             if (HttpContext.Session.GetString("role") == "Teacher")
             {
-                return PartialView("_MainMenu_Student");
+                return PartialView("_MainMenuTeacher");
+            }
+            if (HttpContext.Session.GetString("role") == "Admin")
+            {
+                return PartialView("_MainMenuAdmin");
             }
             else
             {
                 return PartialView("_MainMenu_WithoutRole");
+            }
+
+        }
+        public IActionResult HeaderPartial()
+        {
+            if (HttpContext.Session.GetString("role") == "Student")
+            {
+                return PartialView("_HeaderStudent");
+            }
+            if (HttpContext.Session.GetString("role") == "Teacher")
+            {
+                return PartialView("_HeaderTeacher");
+            }
+            if (HttpContext.Session.GetString("role") == "Admin")
+            {
+                return PartialView("_HeaderAdmin");
+            }
+            else
+            {
+                return PartialView("_HeaderWithoutRole");
             }
 
         }
