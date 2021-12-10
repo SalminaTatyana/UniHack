693727046705815bc4ab7e058ccfc5cd6f9a2a1d
Дмитрий Тюрин <ds.turin73@gmail.com>
2021-12-11 00:59:36 +0400

    Добавил расписание для пользователя

--- UniHackStart/Controllers/TimetableController.cs
+++ UniHackStart/Controllers/TimetableController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniHackStart.Model.Database;
 using UniHackStart.Models;
 
 namespace UniHackStart.Controllers
@@ -15,39 +16,85 @@ namespace UniHackStart.Controllers
         {
             return View();
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> GetTimetable(string n)
         {
+            Student student = new Student();
+            student.GroupId = 10;
+            List<List<StudentTimeTableModel>> timetableWeekList = new List<List<StudentTimeTableModel>>();
+
+
+            using (var db = new UniHackStartDbContext())
+            {
+                List<TimeTableView> timeTableList = db.TimeTableViews.Where(w => w.GroupId == student.GroupId).ToList();
+
+
+                for (int i = 1; i < 3; i++)
+                {
+                    List<StudentTimeTableModel> stundetList = new List<StudentTimeTableModel>();
+                    IOrderedEnumerable<IGrouping<long?, TimeTableView>> ttWeek = timeTableList
+                        .Where(w => w.WeekNumber == i.ToString()).GroupBy(g => g.DayOfWeekId).OrderBy(o => o.Key);
+
+                    foreach (var t in ttWeek)
+                    {
+                        var stud = new StudentTimeTableModel();
+                        stud.DayOfWeek = db.DaysOfWeeks.Where(w => w.Id == t.Key).FirstOrDefault().Name;
+                        stud.ParaList = new List<Para>();
+
+
+                        foreach (TimeTableView item in t)
+                        {
+                            Para p = new Para();
+                            p.TimeStart_1 = item.TimeStart1;
+                            p.TimeEnd_1 = item.TimeEnd1;
+                            p.TimeStart_2 = item.TimeStart2;
+                            p.TimeEnd_2 = item.TimeEnd2;
+                            p.paraNumber = item.ParaNumber;
+                            p.TeacherName = item.Fio;
+                            p.LessonName = item.LessonName;
+                            p.LessonShortType = item.LessonTypeShortName;
+
+                            if (string.IsNullOrEmpty(item.CorpusName))
+                                p.CorpClassRoom = item.ClassRoomName;
+
+                            if (string.IsNullOrEmpty(item.ClassRoomName))
+                                p.CorpClassRoom = item.LessonTypeShortName;
+
+                            if (!string.IsNullOrEmpty(item.CorpusName) && !string.IsNullOrEmpty(item.ClassRoomName))
+                                p.CorpClassRoom = $"{item.CorpusName}-{item.ClassRoomName}";
+
+                            stud.ParaList.Add(p);
+                        }
+
+                        stud.ParaList = stud.ParaList.OrderBy(o => o.paraNumber).ToList();
+                        stundetList.Add(stud);
+                    }
+
+                    timetableWeekList.Add(stundetList);
+                }
+            }
+
             if (n == "1")
             {
-                WeekTimetableModel response = new WeekTimetableModel
+                List<StudentTimeTableModel> WeekOne = timetableWeekList[0];
+                foreach (var c in WeekOne)
                 {
-                    dateOfWeek = "1",
-                    weekNumber = 1,
-                    name = "Электротехника",
-                    teachername = "Шмигирилов Ю.Г.",
-                    time = "8.00-8.45; 18.50-19.35",
-                    place = "3-104"
-                };
-                return PartialView("_partialWeekTimetable", response);
+                    return PartialView("_partialWeekTimetable", c);
+                }
             }
+
             if (n == "2")
             {
-                WeekTimetableModel response = new WeekTimetableModel
+                List<StudentTimeTableModel> WeekOne = timetableWeekList[1];
+                foreach (var c in WeekOne)
                 {
-                    dateOfWeek = "2",
-                    weekNumber = 2,
-                    name = "Электротехника",
-                    teachername = "Шмигирилов Ю.Г.",
-                    time = "8.00-8.45; 18.50-19.35",
-                    place = "3-204"
-                };
-                return PartialView("_partialWeekTimetable", response);
+                    return PartialView("_partialWeekTimetable", c);
+                }
             }
-            else {
-                return PartialView("_partialTimetableAll");
-            }
+
+            return PartialView("_partialWeekTimetable");
+
         }
     }
 }
