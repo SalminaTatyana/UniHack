50d322adef4bade0ca17c0e862f1e1ded1cbd51c
DESKTOP-BMCT3CB\79084 <Maxim300401@mail.ru>
2021-12-11 00:17:23 +0400

    МОДЕЛЬКИ НАХУЙ

---
+++ UniHackStart/Models/DatabaseModel/FacultyLessonDBModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniHackStart.Models.DatabaseModel
+{
+    public class FacultyLessonDBModel
+    {
+    }
+}
