cea2d87891eae67ff5c28fd8649a0eecf42e487f
DESKTOP-BMCT3CB\79084 <Maxim300401@mail.ru>
2021-12-11 06:15:14 +0400

    675756

--- UniHackStart/Models/TeacherModel.cs
+++ UniHackStart/Models/TeacherModel.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace UniHackStart.Models
-{
-    public class Teacher
+{   
+    public class MyTeacher
     {
         public string FIO { get; set; }
-        public string FacultyName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public string FacultyName { get; set; }
+    }
+    public class TeacherModel
+    {
+
+
+        public string FacultyName { get; set; }
+
+        public List<MyTeacher>  MyTeacherList {get;set;}
     }
 }
